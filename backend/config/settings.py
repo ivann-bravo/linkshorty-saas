c8fed7660150ca08677b4991f344e5ae8e9ae1dc
ivann-bravo <ibravo1802@gmail.com>
2025-06-12 01:34:32 -0300

    feat: Docker configurado y funcionando

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)jp07c9rwql25hvra*t13)(8iswqc+#!%0%jr@bnq=q*5ys*3o'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Usa config() para leer la SECRET_KEY y el modo DEBUG
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=0, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -80,8 +79,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASS'),
+        'HOST': config('DB_HOST'),
+        'PORT': 5432, # Puerto por defecto de PostgreSQL
     }
 }
 
--- backend/links/admin.py
+++ backend/links/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Link, Click
 
-# Register your models here.
+class ClickInline(admin.TabularInline):
+    model = Click
+    extra = 0  # No mostrar formularios vacíos adicionales
+    readonly_fields = ('clicked_at', 'ip_address', 'user_agent')
+    can_delete = False  # No permitir borrar clicks desde el admin
+
+@admin.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ('short_code', 'original_url', 'owner', 'created_at')
+    search_fields = ('short_code', 'original_url', 'owner__username')
+    list_filter = ('created_at',)
+    inlines = [ClickInline]
+
+# No es necesario registrar Click por separado si lo mostramos embebido
